6e432545075989a5398b37455205f0bae8e9f066
LizColin9608 <49247014+LizColin9608@users.noreply.github.com>
2024-09-17 23:17:18 -0500

    Clase 1
    Alura

---
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -0,0 +1,53 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById ('contrasena');
+let btnLimpiar = document.getElementById ('limpiar');
+
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!¡@#$%^&*()-_=+\|[]{};:/¿?,.<>';
+
+
+function generar(){
+
+    let numeroDigitado = parseInt (cantidad.value);
+    //console.log(numeroDigitado); //info sacada del código
+
+    if( numeroDigitado < 8){
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+        return;
+    }
+    
+    let password = '';
+
+    for(let i = 0; i < numeroDigitado; i++){
+
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
+
+        password+=caracterAleatorio;
+    }
+
+    //console.log('password generada:' + password);
+
+    contrasena.value = password;
+
+   
+}
+
+function limpiar(){
+    document.getElementById ('contrasena').value = "";
+    document.getElementById ('cantidad').value = "";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
